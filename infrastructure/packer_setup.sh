d5e3ea6e72565c2e3163fc93b25ecc200fd7776f
Thomas Kercheval <spacekattpoispin@gmail.com>
2018-11-05 11:16:24 -0800

    Start integrating gunicorn and nginx

--- infrastructure/packer_setup.sh
+++ infrastructure/packer_setup.sh
@@ -3,12 +3,19 @@
 sudo apt-get update
 sudo apt-get install build-essential nginx python3 -y
 
-sudo python3 -m pip install aiodns
-sudo python3 -m pip install aiohttp
-sudo python3 -m pip install aiofiles
-sudo python3 -m pip install cchardet
+mkdir /etc/tmpfiles.d
+mkdir /etc/systemd/system
+
+#sudo python3 -m pip install aiodns
+#sudo python3 -m pip install aiohttp
+#sudo python3 -m pip install aiofiles
+#sudo python3 -m pip install cchardet
 sudo python3 -m pip install gunicorn
-sudo python3 -m pip install uvloop
+#sudo python3 -m pip install uvloop
+
+sudo chown -R ubuntu /etc/tmpfiles.d
+sudo chown -R ubuntu /etc/systemd/system
+sudo chown -R ubuntu /etc/nginx
 
 sudo systemctl enable nginx.service
 sudo systemctl start nginx.service
