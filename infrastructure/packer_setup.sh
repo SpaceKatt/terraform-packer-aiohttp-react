cef427ceecff2b7a4539b5548ed1fde4d7357b91
Thomas Kercheval <spacekattpoispin@gmail.com>
2018-11-05 09:53:12 -0800

    Update packer build
    Add ssh key
    Add dependencies and nginx

--- infrastructure/packer_setup.sh
+++ infrastructure/packer_setup.sh
@@ -1,3 +1,15 @@
 #/usr/bin/env bash
 
-echo "REEE"
+sudo apt-get update
+sudo apt-get install build-essential nginx python3 -y
+
+sudo python3 -m pip install aiodns
+sudo python3 -m pip install aiohttp
+sudo python3 -m pip install aiofiles
+sudo python3 -m pip install cchardet
+sudo python3 -m pip install gunicorn
+sudo python3 -m pip install uvloop
+
+sudo systemctl enable nginx.service
+sudo systemctl start nginx.service
+
