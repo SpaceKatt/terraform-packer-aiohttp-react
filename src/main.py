963244b5c7124baa0e75add63fa1c8930ccc3338
Thomas Kercheval <spacekattpoispin@gmail.com>
2018-11-05 11:15:23 -0800

    Add basic webserver

---
+++ src/main.py
@@ -0,0 +1,50 @@
+'''
+Runs the webserver.
+'''
+from aiohttp import web
+import aiofiles
+import asyncio
+
+from os import path
+
+
+ROUTES = web.RouteTableDef()
+
+
+@ROUTES.get('/')
+async def root_handle(req):
+    '''
+    Tells the malcontent to go root themselves off our lawn.
+    '''
+    try:
+        file_path = path.join(path.dirname(path.abspath(__file__)),
+                              './static/root.html')
+        async with aiofiles.open(file_path, mode='r') as f:
+            content = await f.read()
+            return web.Response(
+                    body=content,
+                    headers={
+                        'Content-Type': 'text/html'
+                    },
+            )
+    except Exception:
+        return web.Response(status=500)
+
+
+async def init_app():
+    '''
+    Initialize the database, then application server
+    '''
+    app = web.Application()
+
+    app.add_routes(ROUTES)
+
+    return app
+
+
+LOOP = asyncio.get_event_loop()
+APP = LOOP.run_until_complete(init_app())
+
+
+if __name__ == '__main__':
+    web.run_app(APP, host='0.0.0.0')
